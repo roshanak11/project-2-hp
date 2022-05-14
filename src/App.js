0e8228a025b5f924e5ed86594ea8164a6eceba72
roshanak <roshanak.ahmed11@gmail.com>
2022-05-14 17:28:37 +0600

    Commented Everything Out

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-ß// ! Destructuring import syntax!
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+// ß// ! Destructuring import syntax!
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
-import Home from "./components/Home"
-import Characters from "./components/Characters"
-import ShowCharacters from "./components/ShowCharacter"
-import Favorites from "./components/Favorites"
-import Navbar from "./components/Navbar"
+// import Home from "./components/Home"
+// import Characters from "./components/Characters"
+// import ShowCharacters from "./components/ShowCharacter"
+// import Favorites from "./components/Favorites"
+// import Navbar from "./components/Navbar"
 
-function App() {
-  // ! 1) You need a router. You'll pretty always use BrowserRouter
-  // ! 2) You make a Routes component inside.
-  // ! 3) You create all the actual Route components.
-  // ! Those need 2 things: URL path, and the actual component to render.
+// function App() {
+//   // ! 1) You need a router. You'll pretty always use BrowserRouter
+//   // ! 2) You make a Routes component inside.
+//   // ! 3) You create all the actual Route components.
+//   // ! Those need 2 things: URL path, and the actual component to render.
 
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/characters" element={<Characters />} />
-        <Route path="/characters/:showcharacter" element={<SingleCharacter />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </Router>
-  )
-}
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/characters" element={<Characters />} />
+//         <Route path="/characters/:showcharacter" element={<SingleCharacter />} />
+//         <Route path="/favorites" element={<Favorites />} />
+//       </Routes>
+//     </Router>
+//   )
+// }
 
-export default App
+// export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,33 @@
-// ? Import Link component
-import { Link } from "react-router-dom"
-import React from "react"
+// // ? Import Link component
+// import { Link } from "react-router-dom"
+// import React from "react"
 
-// ! The one difference in this navigation, are the Link components.
-function Navbar() {
-  return (
-    <>
-      <header>
-        <nav className="navbar is-dark">
-          <div className="container">
-            <div className="navbar-brand">
-              <Link to="/" className="navbar-item">
-                Home
-              </Link>
-              <Link to="/characters" className="navbar-item">
-                Characters
-              </Link>
-              <Link to="/characters/:showcharacters" className="navbar-item">
-                Show Characters //**UseParam*/
-              </Link>
-              <Link to="/favorites" className="navbar-item">
-                Favourites
-              </Link>
-            </div>
-          </div>
-        </nav>
-      </header>
-    </>
-  )
-}
+// // ! The one difference in this navigation, are the Link components.
+// function Navbar() {
+//   return (
+//     <>
+//       <header>
+//         <nav className="navbar is-dark">
+//           <div className="container">
+//             <div className="navbar-brand">
+//               <Link to="/" className="navbar-item">
+//                 Home
+//               </Link>
+//               <Link to="/characters" className="navbar-item">
+//                 Characters
+//               </Link>
+//               <Link to="/characters/:showcharacters" className="navbar-item">
+//                 Show Characters //**UseParam*/
+//               </Link>
+//               <Link to="/favorites" className="navbar-item">
+//                 Favourites
+//               </Link>
+//             </div>
+//           </div>
+//         </nav>
+//       </header>
+//     </>
+//   )
+// }
 
-export default Navbar
+// export default Navbar
