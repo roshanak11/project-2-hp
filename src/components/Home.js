2b59c4b26e96f1b51061a9cc039234a386e2abc7
jeanzless <jeanzle.albalate208@gmail.com>
2022-05-14 16:05:00 +0100

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,11 @@ import React from "react"
 function Home(){
   return (
     <>
-      <div className='' >
-        <h1>Hello world</h1>
+      <div className='hero is-medium is-primary is-bold' >
+
+        <h1 className='title'>Harry Potter Card List</h1>
+        <h2 className='subtitle'>Interactive Library of all of the Harry Potter characters</h2>
+
       </div>
     </>
   )
