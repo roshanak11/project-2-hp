367ddddfd6350304b1af00bcb179e11d349e322d
roshanak <roshanak.ahmed11@gmail.com>
2022-05-20 01:53:08 +0600

    Character Card Stats

--- src/components/ShowCharacter.js
+++ src/components/ShowCharacter.js
@@ -7,9 +7,18 @@ function ShowCharacter() {
   const character = location.state.character
   return <section className="section">
     <div className="container">
-      <h1>Hello Character Name!</h1>
-      <h2>{character.name}</h2>
-      <h3>{character.species}</h3>
+      <div id="card">
+      <h1>{character.name}</h1>
+      <h2>
+       {character.image && <p><img src={character.image}></img></p>}
+       {character.species && <p>Species: {character.species}</p>}
+       {character.gender && <p>Gender: {character.gender}</p>}
+       {character.ancestry && <p>Ancestry: {character.ancestry}</p>}
+       {character.ancestry && <p>Ancestry: {character.ancestry}</p>}
+       {character.patronus && <p>Patronus: {character.patronus}</p>}
+       {character.wand.core && <p>Wand Core: {character.wand.core}</p>} 
+      </h2>
+      </div>
       <Link to="/characters">{"⬅ Back to all characters"}</Link>
       <CharacterCard {...CharacterCard} />
     </div>
